e6af346050f757e66ed4e23c4e7065d2541f33a8
DONATUS ANKAH <ankahdonatus@gmail.com>
2023-08-18 13:20:58 +0000

    Create agent-session.js

---
+++ dist/js/agent-session.js
@@ -0,0 +1,29 @@
+// Set the time (in milliseconds) after which the user will be automatically logged out
+var logoutTime = 30 * 60 * 1000; //20 minutes
+
+// Start the logout timer
+var logoutTimer = setTimeout(logout, logoutTime);
+
+console.log(logoutTime);
+
+// If the user interacts with the page, reset the timer
+document.addEventListener("mousemove", function() {
+  clearTimeout(logoutTimer);
+  logoutTimer = setTimeout(logout, logoutTime);
+});
+
+document.addEventListener("keydown", function() {
+  clearTimeout(logoutTimer);
+  logoutTimer = setTimeout(logout, logoutTime);
+});
+
+function logout() {
+  firebase.auth().signOut()
+    .then(() => {
+      // User signed out successfully
+      window.location.href = "agent-login";
+    })
+    .catch((error) => {
+      console.error('Logout error:', error);
+    });
+}
